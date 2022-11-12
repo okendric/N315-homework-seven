410c779f166c0f55c9ad934ef127fdb66a294c77
okendric <73384318+okendric@users.noreply.github.com>
2022-11-11 23:57:41 -0500

    Add files via upload

---
+++ app/app.js
@@ -0,0 +1,519 @@
+var LISTS = [
+  {
+    name: "Groceries",
+    listItems: [
+      {
+        name: "milk",
+        checked: false,
+        category: "dairy",
+      },
+      {
+        name: "water",
+        checked: false,
+        category: "dairy",
+      },
+      {
+        name: "bananas",
+        checked: false,
+        category: "fruits",
+      },
+      {
+        name: "cookies",
+        checked: false,
+        category: "fruits",
+      },
+      {
+        name: "bread",
+        checked: false,
+        category: "fruits",
+      },
+      {
+        name: "tomatoes",
+        checked: false,
+        category: "fruits",
+      },
+      {
+        name: "pickles",
+        checked: false,
+        category: "fruits",
+      },
+      {
+        name: "pizza",
+        checked: false,
+        category: "fruits",
+      },
+      {
+        name: "ice cream",
+        checked: false,
+        category: "fruits",
+      },
+      {
+        name: "grapes",
+        checked: false,
+        category: "fruits",
+      },
+    ],
+  },
+  {
+    name: "Electronics",
+    listItems: [
+      {
+        name: "television",
+        checked: false,
+      },
+      {
+        name: "playstation",
+        checked: false,
+      },
+      {
+        name: "xbox Series X",
+        checked: false,
+      },
+      {
+        name: "playstation 5",
+        checked: false,
+      },
+      {
+        name: "nintendo Switch",
+        checked: false,
+      },
+      {
+        name: "iPhone 14 Pro",
+        checked: false,
+      },
+      {
+        name: "samsung Galaxy S22",
+        checked: false,
+      },
+      {
+        name: "laptop",
+        checked: false,
+      },
+      {
+        name: "desktop Computer",
+        checked: false,
+      },
+      {
+        name: "camera",
+        checked: false,
+      },
+    ],
+  },
+  {
+    name: "Clothes",
+    listItems: [
+      {
+        name: "shirt",
+        checked: false,
+      },
+      {
+        name: "jacket",
+        checked: false,
+      },
+      {
+        name: "baseball cap",
+        checked: false,
+      },
+      {
+        name: "knit cap",
+        checked: false,
+      },
+      {
+        name: "dress",
+        checked: false,
+      },
+      {
+        name: "scarf",
+        checked: false,
+      },
+      {
+        name: "coat",
+        checked: false,
+      },
+      {
+        name: "pants",
+        checked: false,
+      },
+      {
+        name: "shorts",
+        checked: false,
+      },
+      {
+        name: "gloves",
+        checked: false,
+      },
+    ],
+  },
+  {
+    name: "Car Parts",
+    listItems: [
+      {
+        name: "tire",
+        checked: false,
+      },
+      {
+        name: "steering wheel cover",
+        checked: false,
+      },
+      {
+        name: "alternator",
+        checked: false,
+      },
+      {
+        name: "battery",
+        checked: false,
+      },
+      {
+        name: "spark plug",
+        checked: false,
+      },
+      {
+        name: "catalytic converter",
+        checked: false,
+      },
+      {
+        name: "radiator",
+        checked: false,
+      },
+      {
+        name: "headlight",
+        checked: false,
+      },
+      {
+        name: "bumper",
+        checked: false,
+      },
+      {
+        name: "gas tank",
+        checked: false,
+      },
+    ],
+  },
+  {
+    name: "Office",
+    listItems: [
+      {
+        name: "pencil",
+        checked: false,
+      },
+      {
+        name: "marker",
+        checked: false,
+      },
+      {
+        name: "paper",
+        checked: false,
+      },
+      {
+        name: "poster",
+        checked: false,
+      },
+      {
+        name: "tape",
+        checked: false,
+      },
+      {
+        name: "scissors",
+        checked: false,
+      },
+      {
+        name: "crayon",
+        checked: false,
+      },
+      {
+        name: "ruler",
+        checked: false,
+      },
+      {
+        name: "protractor",
+        checked: false,
+      },
+      {
+        name: "glue sticks",
+        checked: false,
+      },
+    ],
+  },
+  {
+    name: "Furniture",
+    listItems: [
+      {
+        name: "couch",
+        checked: false,
+      },
+      {
+        name: "office chair",
+        checked: false,
+      },
+      {
+        name: "recliner chair",
+        checked: false,
+      },
+      {
+        name: "barstool",
+        checked: false,
+      },
+      {
+        name: "coffee table",
+        checked: false,
+      },
+      {
+        name: "rug",
+        checked: false,
+      },
+      {
+        name: "desk",
+        checked: false,
+      },
+      {
+        name: "shelf",
+        checked: false,
+      },
+      {
+        name: "painting",
+        checked: false,
+      },
+      {
+        name: "dresser",
+        checked: false,
+      },
+    ],
+  },
+  {
+    name: "Kitchen",
+    listItems: [
+      {
+        name: "fork",
+        checked: false,
+      },
+      {
+        name: "spoon",
+        checked: false,
+      },
+      {
+        name: "knife",
+        checked: false,
+      },
+      {
+        name: "napkins",
+        checked: false,
+      },
+      {
+        name: "spatula",
+        checked: false,
+      },
+      {
+        name: "plate",
+        checked: false,
+      },
+      {
+        name: "bowl",
+        checked: false,
+      },
+      {
+        name: "cup",
+        checked: false,
+      },
+      {
+        name: "frying pan",
+        checked: false,
+      },
+      {
+        name: "whisk",
+        checked: false,
+      },
+    ],
+  },
+  {
+    name: "Medicine",
+    listItems: [
+      {
+        name: "claritin",
+        checked: false,
+      },
+      {
+        name: "tylenol",
+        checked: false,
+      },
+      {
+        name: "eye drops",
+        checked: false,
+      },
+      {
+        name: "equate allergy relief",
+        checked: false,
+      },
+      {
+        name: "benadryl allergy",
+        checked: false,
+      },
+      {
+        name: "hydrogen peroxide",
+        checked: false,
+      },
+      {
+        name: "nyquil",
+        checked: false,
+      },
+      {
+        name: "cough drops",
+        checked: false,
+      },
+      {
+        name: "advil headache reliever",
+        checked: false,
+      },
+      {
+        name: "isopropyl alcohol",
+        checked: false,
+      },
+    ],
+  },
+  {
+    name: "Shoes",
+    listItems: [
+      {
+        name: "tennis shoes",
+        checked: false,
+      },
+      {
+        name: "boots",
+        checked: false,
+      },
+      {
+        name: "sandals",
+        checked: false,
+      },
+      {
+        name: "flip-flops",
+        checked: false,
+      },
+      {
+        name: "high heels",
+        checked: false,
+      },
+      {
+        name: "boat shoes",
+        checked: false,
+      },
+      {
+        name: "crocs",
+        checked: false,
+      },
+      {
+        name: "bowling shoes",
+        checked: false,
+      },
+      {
+        name: "rollerblades",
+        checked: false,
+      },
+      {
+        name: "slippers",
+        checked: false,
+      },
+    ],
+  },
+  {
+    name: "Soda",
+    listItems: [
+      {
+        name: "sprite",
+        checked: false,
+      },
+      {
+        name: "fanta",
+        checked: false,
+      },
+      {
+        name: "coca-cola",
+        checked: false,
+      },
+      {
+        name: "pepsi",
+        checked: false,
+      },
+      {
+        name: "root beer",
+        checked: false,
+      },
+      {
+        name: "dr pepper",
+        checked: false,
+      },
+      {
+        name: "sunkist",
+        checked: false,
+      },
+      {
+        name: "mountain dew",
+        checked: false,
+      },
+      {
+        name: "crush orange",
+        checked: false,
+      },
+      {
+        name: "7up",
+        checked: false,
+      },
+    ],
+  },
+];
+
+function itemChecked(element, listIndex, itemIndex) {
+  $(element).parent().toggleClass("strike");
+  let checkedValue = !LISTS[listIndex].listItems[itemIndex].checked;
+  LISTS[listIndex].listItems[itemIndex].checked = checkedValue;
+}
+
+function addItem(listIndex) {
+  let newItemName = $("#addItem").val();
+  let newItemObj = {
+    name: newItemName,
+    checked: false,
+    category: "",
+  };
+  LISTS[listIndex].listItems.push(newItemObj);
+  loadListItems(listIndex);
+}
+
+function deleteItem(listIndex, idx) {
+  LISTS[listIndex].listItems.splice(idx, 1);
+  loadListItems(listIndex);
+}
+
+function loadListItems(listIndex) {
+  let listString = `<button onclick="loadLists()">Back</button><ul>`;
+  $.each(LISTS[listIndex].listItems, function (idx, listItem) {
+    listString += `<li id="${idx}" class="${
+      listItem.checked ? "strike" : ""
+    }"><input ${
+      listItem.checked ? (checked = "checked") : ""
+    } type="checkbox" id="${idx}" name="${
+      listItem.name
+    }" onclick="itemChecked(this, ${listIndex}, ${idx})"><span>${
+      listItem.name
+    }</span>
+    <span class="delete" onclick="deleteItem(${listIndex}, ${idx})">Delete</span></li>`;
+  });
+  listString += `</ul><div class="addItemInput"><input id="addItem" type="text"><button onclick="addItem(${listIndex})">Add Item</button></div>`;
+
+  $("#app").html(listString);
+}
+
+function loadLists() {
+  let listString = "<ul>";
+  $.each(LISTS, function (idx, list) {
+    listString += `<li id="${idx}" onclick="loadListItems(${idx})">${list.name} <span class="right">Items: ${list.listItems.length}</span></li>`;
+  });
+  listString += "</ul>";
+
+  $("#app").html(listString);
+}
+
+function initListeners() {}
+
+$(document).ready(function () {
+  initListeners();
+});
